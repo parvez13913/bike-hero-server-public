d6537ef5cefceea4275b0728488a7b6b05ded42e
parvez13913 <parvezz13913@gmail.com>
2022-05-26 20:52:10 -0700

    single product api complite

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -22,7 +22,14 @@ async function run() {
             const cursor = productCollection.find(quary);
             const products = await cursor.toArray();
             res.send(products);
-        })
+        });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const products = await productCollection.findOne(query);
+            res.send(products);
+        });
     }
 
     finally {
